0d340a793efefd8bda50444e21a5c1ff69c53477
cjer <dbareket@gmail.com>
2021-05-05 17:25:19 +0300

    add morph_yap command

--- nemo.py
+++ nemo.py
@@ -68,7 +68,24 @@ def write_ncrf_conf(conf_path, input_path, output_path, model, dset):
                 with open(conf_path, 'w', encoding='utf8') as of:
                     for k, v in params.items():
                         of.write(k+'='+str(v)+'\n')        
+    
+            
+def run_yap_hebma(tokens_path, output_path, log_path):
+    result = subprocess.run([YAP_PATH, 'hebma', '-raw', tokens_path, 
+                    '-out', output_path], capture_output=True)
+    with open(log_path, 'wb') as of:
+        of.write(result.stdout)
+    if len(result.stderr)>0:
+        print(result.stderr) 
 
+def run_yap_joint(lattices_path, seg_path, map_path, conll_path, log_path):
+    result = subprocess.run([YAP_PATH, 'joint', '-in', lattices_path, 
+                    '-os', seg_path, '-om', map_path, '-oc', conll_path], capture_output=True)
+    with open(log_path, 'wb') as of:
+        of.write(result.stdout)
+    if len(result.stderr)>0:
+        print(result.stderr) 
+        
         
 def run_ncrf_main(conf_path, device, log_path):
     result = subprocess.run(['python', 'ncrf_main.py', '--config', conf_path, 
@@ -98,7 +115,40 @@ def run_ner_model(model_name, input_path, output_path):
                 os.remove(path)
                 
 def run_morph_yap(model_name, input_path, output_path):
-    pass
+    temp_tokens_path = os.path.join(LOCAL_TEMP_FOLDER, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')+'.txt')
+    temp_conf_path = temp_tokens_path.replace('.txt','_ncrf.conf')
+    temp_yap_log_path = temp_tokens_path.replace('.txt','_yap.log')
+    temp_lattices_path = temp_tokens_path.replace('.txt','.lattices')
+    temp_seg_path = temp_tokens_path.replace('.txt','.seg')
+    temp_map_path = temp_tokens_path.replace('.txt','.map')
+    temp_conll_path = temp_tokens_path.replace('.txt','.conll')
+    temp_ncrf_morph_input = temp_tokens_path.replace('.txt', '_yapform.txt')
+    temp_ncrf_log_path = temp_tokens_path.replace('.txt','_ncrf.log')
+    try:
+        # read file and tokenize
+        sents = read_text_file(input_path)
+        #write temporary tokens file for yap
+        write_tokens_file(sents, temp_tokens_path, only_tokens=True)
+        #run yap hebma to create ambiguous lattices
+        run_yap_hebma(temp_tokens_path, temp_lattices_path, temp_yap_log_path)
+        #run yap joint
+        run_yap_joint(temp_lattices_path, temp_seg_path, temp_map_path, temp_conll_path, temp_yap_log_path)
+        #create temp morph input with dummy o
+        lat = bclm.read_conll(temp_conll_path)
+        form_sents = lat.groupby('sent_id').form.apply(lambda x: x.tolist()).tolist()
+        write_tokens_file(form_sents, temp_ncrf_morph_input, dummy_o=True)
+        #run ncrf morph model
+        write_ncrf_conf(temp_conf_path, temp_ncrf_morph_input, output_path, MODEL_PATHS[model_name]['model'], MODEL_PATHS[model_name]['dset'])
+        run_ncrf_main(temp_conf_path, DEVICE, temp_ncrf_log_path)
+    except Exception as e:
+        print(traceback.format_exc())
+    #delete all temp files
+    if DELETE_TEMP_FILES:
+        for path in [temp_tokens_path, temp_conf_path, temp_yap_log_path, temp_lattices_path, 
+                     temp_seg_path, temp_map_path, temp_conll_path,
+                     temp_ncrf_morph_input, temp_ncrf_log_path]:
+            if os.path.exists(path):
+                os.remove(path)
 
                 
 def run_morph_hybrid(model_name, input_path, output_path):
@@ -111,6 +161,8 @@ def run_multi_hybrid(model_name, input_path, output_path):
 def run_morph_tok(model_name, input_path, output_path):
     pass
 
+ 
+    
     
 if __name__=='__main__':
     command = sys.argv[1]
@@ -144,5 +196,4 @@ if __name__=='__main__':
     #run morph model on hybrid segmented output and align back with tokens
     if command=='morph_hybrid_token':
         run_morph_hybrid_token(model_name, input_path, output_path)
-        
-        
+
