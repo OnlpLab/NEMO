f4448f24878d72d20d7d0da8ed1781a85861c190
cjer <9987721+cjer@users.noreply.github.com>
2021-09-05 12:56:18 +0300

    suppress iobes warnings

--- api_main.py
+++ api_main.py
@@ -15,8 +15,9 @@ from io import StringIO
 from operator import itemgetter
 from itertools import groupby
 import iobes
-from logging import ERROR
-iobes.LOGGER.setLevel(level=ERROR)
+if SUPPRESS_IOBES_WARNINGS:
+    from logging import ERROR
+    iobes.LOGGER.setLevel(level=ERROR)
 
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
